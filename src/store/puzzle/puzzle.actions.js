6bf9b7f930942df99dc99c96c84050339d23c368
Tim Bakkum <timbakkum@gmail.com>
2019-05-27 10:34:07 +0200

    chore: shortens action creators

--- src/store/puzzle/puzzle.actions.js
+++ src/store/puzzle/puzzle.actions.js
@@ -1,50 +1,36 @@
 import * as constants from "./puzzle.constants";
 
-export const shufflePieces = () => {
-  return {
-    type: constants.SHUFFLE_PIECES
-  };
-};
+export const shufflePieces = () => ({
+  type: constants.SHUFFLE_PIECES
+});
 
-export const startPuzzle = () => {
-  return {
-    type: constants.START_PUZZLE
-  };
-};
+export const startPuzzle = () => ({
+  type: constants.START_PUZZLE
+});
 
-export const startEasyMode = () => {
-  return {
-    type: constants.START_EASY_MODE
-  };
-};
+export const startEasyMode = () => ({
+  type: constants.START_EASY_MODE
+});
 
-export const resetPuzzle = () => {
-  return {
-    type: constants.RESET_PUZZLE
-  };
-};
+export const resetPuzzle = () => ({
+  type: constants.RESET_PUZZLE
+});
 
-export const movePiece = pieceIndex => {
-  return {
-    type: constants.MOVE_PIECE,
-    payload: {
-      pieceIndex
-    }
-  };
-};
+export const movePiece = pieceIndex => ({
+  type: constants.MOVE_PIECE,
+  payload: {
+    pieceIndex
+  }
+});
 
-export const swapPiece = (pieceIndex, emptyIndex) => {
-  return {
-    type: constants.SWAP_PIECE,
-    payload: {
-      pieceIndex,
-      emptyIndex
-    }
-  };
-};
+export const swapPiece = (pieceIndex, emptyIndex) => ({
+  type: constants.SWAP_PIECE,
+  payload: {
+    pieceIndex,
+    emptyIndex
+  }
+});
 
-export const winPuzzle = () => {
-  return {
-    type: constants.WIN_PUZZLE
-  };
-};
+export const winPuzzle = () => ({
+  type: constants.WIN_PUZZLE
+});

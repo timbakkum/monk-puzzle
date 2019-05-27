cfb494cadb2b6d139e37697858ba530f03d52aa8
Tim Bakkum <timbakkum@gmail.com>
2019-05-27 10:58:28 +0200

    chore: refactors Piece computed styles

--- src/features/Puzzle/Piece.js
+++ src/features/Puzzle/Piece.js
@@ -4,17 +4,20 @@ import { useSpring, animated } from "react-spring";
 import { PUZZLE_SIZE, PUZZLE_COLUMNS, PUZZLE_ROWS } from "../../constants";
 import monksImage from "./monks.jpg";
 
+const pieceWidth = PUZZLE_SIZE / PUZZLE_ROWS;
+const pieceHeight = PUZZLE_SIZE / PUZZLE_COLUMNS;
+
 const StyledPiece = styled.div`
   position: absolute;
   box-shadow: ${props =>
     props.isEmpty ? "none" : "inset 0px 0px 0px 1px rgba(0, 0, 0, 1)"};
   text-align: center;
-  width: ${PUZZLE_SIZE / PUZZLE_COLUMNS}px;
-  height: ${PUZZLE_SIZE / PUZZLE_ROWS}px;
+  width: ${pieceWidth}px;
+  height: ${pieceHeight}px;
   background-image: url(${props => (props.isEmpty ? "none" : monksImage)});
   background-repeat: no-repeat;
-  background-position: ${props => props.intendedColumn * -125}px
-    ${props => props.intendedRow * -125}px;
+  background-position: ${props => props.intendedColumn * -pieceWidth}px
+    ${props => props.intendedRow * -pieceHeight}px;
   opacity: ${props => (props.isActive ? 1 : 0.5)};
   transition: opacity 0.5s ease;
   cursor: ${props => (props.isActive ? "pointer" : "not-allowed")};
